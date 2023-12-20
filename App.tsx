a438e3808bf66e0645771db3a785d038b35d0635
InDaviX <dszewczyk235@gmail.com>
2023-12-20 20:21:32 +0100

    Obrazki i obsługa errorów

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Image} from 'react-native';
 
 const App = () => {
+  const [imageSource, setImageSource] = useState({
+    uri: 'https://i.pinimg.com/736x/fa/ff/1a/faff1ae99e9a694e40e3c9d3f2a17cdc.jpg',
+  });
   return (
     <SafeAreaView>
-      <Text>Hello World!</Text>
+      <Image
+        source={require('./assets/images/cake.png')}
+        style={{width: 100, height: 100}}
+      />
+      <Image
+        source={imageSource}
+        style={{width: '100%', height: 400, backgroundColor: 'darkgray'}}
+        resizeMode={'contain'}
+        onError={() => {
+          console.log('error');
+          setImageSource(require('./assets/images/error.png'));
+        }}
+      />
     </SafeAreaView>
   );
 };
